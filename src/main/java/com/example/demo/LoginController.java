57e013f35be03e80fe1e880c8dbf3be6d88d69bb
Eswarraja-14 <eswarraja.27csa@licet.ac.in>
2026-04-27 15:34:46 +0530

    Update LoginController.java

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import org.springframework.stereotype.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
